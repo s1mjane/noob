6b58aed81379a5c0efc3bf287ef31bbabf4df533
s1mjane <s1mjane@handong.ac.kr>
2024-08-22 17:29:10 +0900

    login & signup UI

---
+++ noobie/android/app/src/main/kotlin/com/example/noobie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.noobie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ noobie/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:noobie/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: LoginSignupScreen(),
+    );
+  }
+}
